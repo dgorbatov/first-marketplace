025a66c5c34c8a10461645d5fccb058b6d31a6a0
Daniel Gorbatov <dg20@uw.edu>
2021-09-10 19:13:47 -0700

    Team

--- src/main/team/team.js
+++ src/main/team/team.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
 import "./team.css";
 import config from "../../extra/config";
 import { initializeApp } from "@firebase/app";
@@ -10,20 +10,39 @@ const db = getFirestore();
 
 function Team() {
   const { id } = useParams();
+  const [team, setTeam] = useState(null);
+  const history = useHistory();
 
   useEffect(() => {
     async function getData() {
       const docSnap = await getDoc(doc(db, "user-info/" + id + "/profile/info"))
-      console.log(docSnap.data());
+      if (!(docSnap.exists())) {
+        history.push("/error/404");
+      }
+      setTeam(docSnap.data());
     }
 
     getData();
-  }, [id]);
+  }, [id, history]);
 
 
   return (
     <div className="team">
-      <p>Hello World!</p>
+      {team !== null && <article>
+        <section className="pfp">
+          <img src={team.pfp} alt="Team PFP" />
+          <p>{team.name}</p>
+        </section>
+
+        <section className="team-info-page">
+          <h1>Info:</h1>
+          <p>Location: {team.city}, {team.country}</p>
+          <p>Competition: {team.comp}</p>
+          <p>Rookie year: {team.creat} </p>
+          <p>Number of Team Members: {team["num-mem"]}</p>
+          <p>Team Number: {team.number}</p>
+        </section>
+      </article>}
     </div>
   );
 }
